c3a7a19a8555d80c0b8a4c7fdd4826f741ef3f2d
Oliver <two216@qq.com>
2019-04-12 17:54:32 +0800

    __init__

---
+++ SingleLink.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#author: Gong
+'''
+This is a simple single link list implemented in python.
+
+'''
+class SingleLinkList:
+    def __init__(self,header = None):
+        self.header = header
+
+    def travel(self):
+        pass
+
+    def append(self):
+        pass
+
+    def travel(self):
+        pass
+
+
